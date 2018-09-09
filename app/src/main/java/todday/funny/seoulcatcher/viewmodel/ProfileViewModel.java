6c50dee5e83781bc8046d8cb88afcf58f702aae2
ddoniddoni <psdkei@naver.com>
2018-09-09 17:46:57 +0900

    원균이랑 같이 코드분석

--- app/src/main/java/todday/funny/seoulcatcher/viewmodel/ProfileViewModel.java
+++ app/src/main/java/todday/funny/seoulcatcher/viewmodel/ProfileViewModel.java
@@ -3,7 +3,18 @@ package todday.funny.seoulcatcher.viewmodel;
 import android.content.Context;
 
 public class ProfileViewModel extends BaseViewModel {
+
+     public String a = "ㅎㅎ";
+
     public ProfileViewModel(Context context) {
         super(context);
+
+
+
+    }
+
+    public void setA(String a){
+        this.a = a;
     }
 }
+
