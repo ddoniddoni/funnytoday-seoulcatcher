70650226f48453dfc43fbc3f6033ae1b20ac6828
yts <sky877kr@gmail.com>
2018-09-11 14:52:16 +0900

    1.스케쥴 모듈 변경

--- app/src/main/java/todday/funny/seoulcatcher/model/Schedule.java
+++ app/src/main/java/todday/funny/seoulcatcher/model/Schedule.java
@@ -1,9 +1,14 @@
 package todday.funny.seoulcatcher.model;
 
-public class Schedule {
-    private String date;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public Schedule(){}
+public class Schedule implements Parcelable {
+    private String date;
+    private String name;
+
+    public Schedule() {
+    }
 
     public Schedule(String date) {
         this.date = date;
@@ -17,5 +22,40 @@ public class Schedule {
         this.date = date;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.date);
+        dest.writeString(this.name);
+    }
+
+    protected Schedule(Parcel in) {
+        this.date = in.readString();
+        this.name = in.readString();
+    }
+
+    public static final Parcelable.Creator<Schedule> CREATOR = new Parcelable.Creator<Schedule>() {
+        @Override
+        public Schedule createFromParcel(Parcel source) {
+            return new Schedule(source);
+        }
+
+        @Override
+        public Schedule[] newArray(int size) {
+            return new Schedule[size];
+        }
+    };
 }
