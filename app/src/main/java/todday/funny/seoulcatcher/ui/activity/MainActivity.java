6d73faba4edb066068c55f067265f6527c1e9004
yts <sky877kr@gmail.com>
2018-09-11 09:51:08 +0900

    1. MainViewModel 변경

--- app/src/main/java/todday/funny/seoulcatcher/ui/activity/MainActivity.java
+++ app/src/main/java/todday/funny/seoulcatcher/ui/activity/MainActivity.java
@@ -26,79 +26,13 @@ public class MainActivity extends BaseActivity {
     private MainBinding binding;
     private MainViewModel model;
 
-    private FragmentManager fragmentManager;
-    private FragmentTransaction fragmentTransaction;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         model = getMainViewModel();
-        model.setNavigationItemSelectedListener(navigationItemSelectedListener);
+        model.setNavigationItemSelectedListener();
         binding.setModel(model);
-
-        replaceFramgment(ProfileFragment.newInstance());
-    }
-
-    private void init(){
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.container, model.getScheduleFragment(), "schedule");
-        fragmentTransaction.add(R.id.container, model.getEducationFragment(), "education");
-        fragmentTransaction.add(R.id.container, model.getProfileFragment(), "profile");
-        fragmentTransaction.add(R.id.container, model.getHistoryFragment(), "history");
-        fragmentTransaction.add(R.id.container, model.getSettingFragment(), "setting");
-
-        fragmentTransaction.hide(model.getScheduleFragment());
-        fragmentTransaction.hide(model.getEducationFragment());
-        fragmentTransaction.hide(model.getHistoryFragment());
-        fragmentTransaction.hide(model.getSettingFragment());
-        fragmentTransaction.commit();
-    }
-
-    public void replaceFramgment(Fragment fragment) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragment)
-                .commit();
-    }
-
-    public BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.navigation_educationSchedule:
-                    replaceFramgment(ScheduleFragment.newInstance());
-                    return true;
-                case R.id.navigation_education:
-                    replaceFramgment(EducationFragment.newInstance());
-                    return true;
-                case R.id.navigation_profile:
-                    replaceFramgment(ProfileFragment.newInstance());
-                    return true;
-                case R.id.navigation_history:
-                    replaceFramgment(HistoryFragment.newInstance());
-                    return true;
-                case R.id.navigation_setting:
-                    replaceFramgment(SettingFragment.newInstance());
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    private void selectView(String tag){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        @SuppressLint("RestrictedApi") List<Fragment> fragments = fragmentManager.getFragments();
-        for (Fragment fragment : fragments) {
-            if (fragment.getTag().equals(tag)) {
-                fragmentTransaction.show(fragment);
-            } else {
-                fragmentTransaction.hide(fragment);
-            }
-        }
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.commit();
+        model.initFragmentList(R.id.container);
     }
 }
--- app/src/main/java/todday/funny/seoulcatcher/viewmodel/MainViewModel.java
+++ app/src/main/java/todday/funny/seoulcatcher/viewmodel/MainViewModel.java
@@ -1,10 +1,19 @@
 package todday.funny.seoulcatcher.viewmodel;
 
 import android.content.Context;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
+import todday.funny.seoulcatcher.R;
+import todday.funny.seoulcatcher.ui.activity.MainActivity;
 import todday.funny.seoulcatcher.ui.fragment.EducationFragment;
 import todday.funny.seoulcatcher.ui.fragment.HistoryFragment;
 import todday.funny.seoulcatcher.ui.fragment.ProfileFragment;
@@ -12,42 +21,90 @@ import todday.funny.seoulcatcher.ui.fragment.ScheduleFragment;
 import todday.funny.seoulcatcher.ui.fragment.SettingFragment;
 
 public class MainViewModel extends BaseViewModel {
-
-    private ScheduleFragment scheduleFragment = ScheduleFragment.newInstance();
-    private EducationFragment educationFragment = EducationFragment.newInstance();
-    private ProfileFragment profileFragment = ProfileFragment.newInstance();
-    private HistoryFragment historyFragment = HistoryFragment.newInstance();
-    private SettingFragment settingFragment = SettingFragment.newInstance();
+    private ArrayList<Fragment> mainFragmentList = new ArrayList<>();
+    private ObservableField<String> mCurrentTag = new ObservableField<>();
 
     public BottomNavigationView.OnNavigationItemSelectedListener mNavigationItemSelectedListener;
-
 
     public MainViewModel(Context context) {
         super(context);
     }
 
-    public void setNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener mNavigationItemSelectedListener) {
-        this.mNavigationItemSelectedListener = mNavigationItemSelectedListener;
+    public void setNavigationItemSelectedListener() {
+        this.mNavigationItemSelectedListener = navigationItemSelectedListener;
     }
 
-    public ScheduleFragment getScheduleFragment() {
-        return scheduleFragment;
+    private void setCurrentTag(String tag) {
+        mCurrentTag.set(tag);
     }
 
-    public EducationFragment getEducationFragment() {
-        return educationFragment;
+    public void initFragmentList(int id) {
+        mainFragmentList.add(ScheduleFragment.newInstance());
+        mainFragmentList.add(EducationFragment.newInstance());
+        mainFragmentList.add(ProfileFragment.newInstance());
+        mainFragmentList.add(HistoryFragment.newInstance());
+        mainFragmentList.add(SettingFragment.newInstance());
+        if (mContext instanceof AppCompatActivity) {
+            FragmentManager fragmentManager = ((AppCompatActivity) mContext).getSupportFragmentManager();
+            if (fragmentManager != null) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                for (Fragment fragment : mainFragmentList) {
+                    fragmentTransaction.add(id, fragment, fragment.getClass().getSimpleName());
+                    fragmentTransaction.hide(fragment);
+                }
+                setCurrentTag(ProfileFragment.class.getSimpleName());
+                fragmentTransaction.show(mainFragmentList.get(2));
+                fragmentTransaction.commit();
+            }
+        }
     }
 
-    public ProfileFragment getProfileFragment() {
-        return profileFragment;
+    public void moveFragment(String tag) {
+        if (tag != null) {
+            if (mCurrentTag.get() != null && !mCurrentTag.get().equals(tag)) {
+                if (mContext instanceof MainActivity) {
+                    mCurrentTag.set(tag);
+                    FragmentManager fragmentManager = ((MainActivity) mContext).getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    for (Fragment fragment : fragmentManager.getFragments()) {
+                        String fTag = fragment.getTag();
+                        if (fTag != null) {
+                            if (fTag.equals(tag)) {
+                                fragmentTransaction.show(fragment);
+                            } else {
+                                fragmentTransaction.hide(fragment);
+                            }
+                        }
+                    }
+                    fragmentTransaction.setReorderingAllowed(true);
+                    fragmentTransaction.commit();
+                }
+            }
+        }
     }
 
-    public HistoryFragment getHistoryFragment() {
-        return historyFragment;
-    }
-
-    public SettingFragment getSettingFragment() {
-        return settingFragment;
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_educationSchedule:
+                    moveFragment(ScheduleFragment.class.getSimpleName());
+                    return true;
+                case R.id.navigation_education:
+                    moveFragment(EducationFragment.class.getSimpleName());
+                    return true;
+                case R.id.navigation_profile:
+                    moveFragment(ProfileFragment.class.getSimpleName());
+                    return true;
+                case R.id.navigation_history:
+                    moveFragment(HistoryFragment.class.getSimpleName());
+                    return true;
+                case R.id.navigation_setting:
+                    moveFragment(SettingFragment.class.getSimpleName());
+                    return true;
+            }
+            return false;
+        }
+    };
 
 }
